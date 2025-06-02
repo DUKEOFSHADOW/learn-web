1e79c9dc61a0ca490afdf27ad8e2b578ba55bf82
DUKEOFSHADOW <3210672225@qq.com>
2025-06-02 12:18:09 +0800

    初始化项目

---
+++ vue/todo-vue/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <header>
+    <div id="app">
+      <h1>To-do List</h1>
+    </div>
+  </header>
+
+  
+</template>
+
+<!-- <style scoped>
+/*scoped属性将样式的范围限制到单个文件组件的内容里*/
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style> -->
