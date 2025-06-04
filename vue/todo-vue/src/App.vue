9124ed554f6e7671f5602972a721f03b3f1307e9
DUKEOFSHADOW <3210672225@qq.com>
2025-06-04 14:55:36 +0800

    创建一个vue待办事项列表

--- vue/todo-vue/src/App.vue
+++ vue/todo-vue/src/App.vue
@@ -1,16 +1,43 @@
 <script setup>
+import ToDoItem from './components/ToDoItem.vue';
+import { nanoid } from "nanoid";
 
+// 组件名称可以通过文件名推断，或者在构建工具中配置
+// components 选项不再需要，因为在 <script setup> 中导入的组件会自动注册
+// data 选项中的响应式数据可以直接使用 ref 或 reactive 定义
+const ToDoItems = [
+        { id: "todo-" + nanoid(),
+          label: "Learn Vue",
+          done: false,
+        },
+        {
+          id: "todo-" + nanoid(),
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        { id: "todo-" + nanoid(),
+          label: "Have fun",
+          done: true,
+        },
+        {
+          id: "todo-" + nanoid(),
+          label: "Create a to-do list",
+          done: false,
+        },
+];
 
 </script>
 
 <template>
-  <header>
     <div id="app">
       <h1>To-do List</h1>
+      <ul>
+        <li v-for="item in ToDoItems" :key="item.id">
+          <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+        </li>
+      </ul>
     </div>
-  </header>
 
-  
 </template>
 
 <!-- <style scoped>
