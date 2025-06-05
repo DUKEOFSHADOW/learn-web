4029f2acded277003467fdffde737832e3b8f2db
DUKEOFSHADOW <3210672225@qq.com>
2025-06-05 14:51:04 +0800

    添加css文件reset.css

--- vue/todo-vue/src/App.vue
+++ vue/todo-vue/src/App.vue
@@ -1,43 +1,41 @@
 <script setup>
-import ToDoItem from './components/ToDoItem.vue';
+import { ref } from 'vue';
 import { nanoid } from "nanoid";
+import ToDoItem from './components/ToDoItem.vue';
+import ToDoForm from './components/ToDoForm.vue';
 
-// 组件名称可以通过文件名推断，或者在构建工具中配置
-// components 选项不再需要，因为在 <script setup> 中导入的组件会自动注册
-// data 选项中的响应式数据可以直接使用 ref 或 reactive 定义
-const ToDoItems = [
-        { id: "todo-" + nanoid(),
-          label: "Learn Vue",
-          done: false,
-        },
-        {
-          id: "todo-" + nanoid(),
-          label: "Create a Vue project with the CLI",
-          done: true,
-        },
-        { id: "todo-" + nanoid(),
-          label: "Have fun",
-          done: true,
-        },
-        {
-          id: "todo-" + nanoid(),
-          label: "Create a to-do list",
-          done: false,
-        },
-];
+// 定义响应式数据 ToDoItems
+const ToDoItems = ref([
+  { id: "todo-" + nanoid(), label: "Learn Vue", done: false },
+  {
+    id: "todo-" + nanoid(),
+    label: "Create a Vue project with the CLI",
+    done: true,
+  },
+  { id: "todo-" + nanoid(), label: "Have fun", done: true },
+  {
+    id: "todo-" + nanoid(),
+    label: "Create a to-do list",
+    done: false,
+  },
+]);
 
+// 定义 addToDo 方法
+const addToDo = (toDoLabel) => {
+  ToDoItems.value.push({id: "todo-" + nanoid(), label: toDoLabel, done: false});
+};
 </script>
 
 <template>
     <div id="app">
       <h1>To-do List</h1>
+      <to-do-form @todo-added="addToDo"></to-do-form>
       <ul>
         <li v-for="item in ToDoItems" :key="item.id">
           <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
         </li>
       </ul>
     </div>
-
 </template>
 
 <!-- <style scoped>
